d990e8012ae82dd2cbf4454cf643aa8efe0974bf
Andrey Yashnev <andjash@gmail.com>
2016-08-14 19:55:47 +0400

    Refactored fadein segue

--- TweetSpot/Classes/Utils/Segues/FadeInSegue.swift
+++ TweetSpot/Classes/Utils/Segues/FadeInSegue.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
+
+private var associationHandle: UInt8 = 0
+
 class FadeInSegue: UIStoryboardSegue, UIViewControllerTransitioningDelegate {
     
-    override func perform() {
-        objc_setAssociatedObject(self, "FadeInSegue.DismissAnimator", self, .OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-        destinationViewController.transitioningDelegate = self
+     override func perform() {
+        let delegate = FadeTransitionDelegate()
+        objc_setAssociatedObject(destinationViewController, &associationHandle, delegate, .OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        destinationViewController.transitioningDelegate = delegate
         sourceViewController.presentViewController(self.destinationViewController, animated: true, completion: nil)
     }
+}
+
+private class FadeTransitionDelegate: NSObject, UIViewControllerTransitioningDelegate {
     
-    // MARK: UIViewControllerTransitioningDelegate
-    
-    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    @objc func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return FadeAnimator(presenting: true)
     }
     
-    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return FadeAnimator(presenting: false, completion: { [unowned self] in
-            objc_removeAssociatedObjects(self)
+    @objc func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return FadeAnimator(presenting: false, completion: {
+            objc_setAssociatedObject(dismissed,  &associationHandle, nil, .OBJC_ASSOCIATION_RETAIN_NONATOMIC);
         })
     }
 }
